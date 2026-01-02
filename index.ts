ff04d5bbbf3925a56cc9340a59c9a2510f8c33e4
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-01-02 11:52:02 +0530

    feat: Add Mongoose-based MongoDB connection and initialize it in the backend entry point.

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./src/config/db";
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL || "*",
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 8000;
-
+connectDB();
 app.get("/", (req, res) => {
   console.log("start a server");
   res.send("Hello World!");
