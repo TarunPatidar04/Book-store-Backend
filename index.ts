3fb4d184c9005b4188c2df7d0cabf6ccd4eb2ccf
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-01-02 11:42:22 +0530

    feat: Initialize backend project with essential configurations and a basic Express server setup.

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "*",
+  credentials: true,
+};
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cookieParser());
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  console.log("start a server");
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
