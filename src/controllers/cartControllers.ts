b3d33b5e218b31689377addf37102d6833dec328
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-01-06 13:47:05 +0530

    feat: Implement cart model and add to cart controller.

---
+++ src/controllers/cartControllers.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import Products from "../models/Product";
+import { responseHandler } from "../utils/responseHandler";
+import { CartItems, ICartItem } from "../models/CartItems";
+
+export const addToCart = async (req: Request, res: Response) => {
+  try {
+    const { productId, quantity } = req.body;
+    const userId = req.id;
+
+    const product = await Products.findById(productId);
+
+    if (!product) {
+      return responseHandler(res, 404, "Product not found");
+    }
+
+    if (product.seller.toString() === userId) {
+      return responseHandler(
+        res,
+        400,
+        "You cannot add your own product to the cart"
+      );
+    }
+
+    let cart = await CartItems.findOne({ user: userId });
+    if (!cart) {
+      cart = new CartItems({ user: userId, items: [] });
+    }
+    const existingItems = cart.items.find(
+      (item) => item.product.toString() === productId
+    );
+    if (existingItems) {
+      existingItems.quantity += quantity;
+    } else {
+      const newItem = { product: productId, quantity: quantity };
+      cart.items.push(newItem as ICartItem);
+    }
+    await cart.save();
+    return responseHandler(res, 200, "Product added to cart");
+  } catch (error) {
+    console.log(error);
+    return responseHandler(res, 500, "Internal Server Error");
+  }
+};
