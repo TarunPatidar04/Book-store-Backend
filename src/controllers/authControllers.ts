2f7c6a641c6852edebb8a4973b8ed948bf6c51b0
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-01-26 13:50:03 +0530

    fix: correctly extract token from request parameters in reset password.

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -132,7 +132,7 @@ export const forgotPassword = async (req: Request, res: Response) => {
 
 export const resetPassword = async (req: Request, res: Response) => {
   try {
-    const token = req.params;
+    const {token} = req.params;
     const { newPassword } = req.body;
 
     const user = await UserModel.findOne({
